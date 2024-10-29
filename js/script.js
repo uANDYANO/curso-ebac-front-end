679681686b940bbec7b0661ee6e031ee8441202f
Anderson Salviano <andyanodev@gmail.com>
2024-10-29 10:12:56 -0300

    commit inicial

---
+++ js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    $('#telefone').mask('(00) 00000-0000', {
+        placeholder: '(DDD) 12345-6789'
+    })
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            endereco: {
+                required: true
+            },
+            cep: {
+                required: true
+            },
+            cpf: {
+                required: true
+            },
+        },
+        submitHandler: function (form) {
+            alert("Seu cadastro foi efetuado com sucesso!");
+            form.reset();
+        },
+        invalidHandler: function (form, validator) {
+            alert("Por favor, preencha todos os campos!");
+        }
+    })
+})
